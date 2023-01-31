a3198967da302435aa54a110496578b97e24b40a
Administrator <26501808792@qq.com>
2023-01-31 17:02:59 +0800

    2023.1.31 完成了登录界面设计

---
+++ App.vue
@@ -0,0 +1,58 @@
+<script>
+	export default {
+		globalData : {
+			//用户信息全部变量
+			//名字
+			name:['登陆后显示个人信息'],
+			//焊工为焊工号，其它人员为身份标识码
+			userId:[''],
+			//单位--subcontractor
+			subcontractor: [''],
+			email:[''],
+			phoneNumber:[],
+			passWord:[],
+			//权限
+			userClassCode: [],
+			
+			//上线测试
+			//url : ['http://1.116.137.136:8080/'],
+			//本地测试
+			url : ['http://127.0.0.1:8080/'],
+			
+		},
+		
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+.whitedivide {/*白色分割线*/
+  height: 1px;
+  background-color: #FFFFFF;
+  width: 56px;
+  position:relative;
+  top: 0px;
+}
+
+.graydivide {/*灰色分割条*/
+  height: 10px;
+  background-color: #ededed;
+  position:relative;
+  top: 0px;
+}
+.blanke{
+height: 10px;
+display: flex;
+background-color: #ffffff;
+}
+
+	/*每个页面公共css */
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,137 @@
+<template>
+	<view class="container">
+		<view class="title-box">
+            <view class="text_title">欢迎使用</view>
+		    <view class="text_title">报检信息系统</view>
+		</view>
+		<view class="login-box">
+			<view class="input-box">
+				<image class="nameImage" src="../image/name.png"></image>
+				<label class="loginLab">账号</label>
+				<input class="inputText" placeholder="请输入手机号" v-model="phonenumber" /> 
+			</view>
+			<view class="whitedivide"></view>
+			<view class="input-box">
+				<image class="keyImage" src="../image/key.png"></image>
+                <label class="loginLab">密码</label> 
+                <input class="inputText" password="true" placeholder="请输入密码" v-model ="password" />
+			</view>
+		</view>
+		
+		<!--按钮--> 
+		<view class="button-box">
+			<button class = "buttonMain" @click="regisFun">注册</button>
+			<button class = "buttonMain" @click="loginFun">登录</button> 
+		</view> 
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				phonenumber : [],
+				password : [],
+					
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			loginFun(e){
+				uni.request({
+					url: getApp().globalData.url,
+					data:{
+						strUser : this.phonenumber,
+						strPwd : this.password
+					},
+					success: (res) => {
+					        console.log(res.data);
+					        this.text = 'request success';
+					    }
+				})
+				
+			},
+			regisFun(e){
+				
+			
+		}
+	}
+</script>
+
+
+<style>
+.container{
+	height: 100%;
+	width: 100%;
+	background-color: #FFFFFF;
+	display: flex;
+	flex-direction: column;
+	
+}
+
+.title-box{
+	display: flex;
+	flex-direction: column;
+	height: 30vh;
+	
+}
+
+.login-box{
+	padding-top: 5px;
+	height: auto;
+	display: flex;
+	flex-direction: column;
+	
+}
+
+.text_title{
+  text-align: center;
+  padding: 10px 10px;
+  color: black; 
+  font-size: 28px 
+}
+.input-box{
+	height: 40px;
+	display: flex;
+	flex-direction: row;
+	margin: 0 20px;
+	background-color: #ededed;
+}
+
+.nameImage, .keyImage { 
+  margin: 10px 10px; 
+  width: 20px; 
+  height: 20px 
+} 
+.loginLab { 
+  margin: 10px 10px; 
+  color: #545454; 
+  font-size: 14px 
+} 
+.inputText { 
+  flex: block; 
+  float: right; 
+  text-align: left; 
+  margin: 10px 10px;
+  color: black; 
+  font-size: 14px } 
+
+.button-box { 
+  display: flex;
+  flex-direction: column-reverse;
+  width: 100%; 
+  height: auto;
+  margin-top: 10px; 
+  margin-bottom: 20px; 
+  padding-bottom: 0px;
+   
+} 
+
+.buttonMain{
+  width: 60%; 
+  margin-top: 15px; 
+  background-color: #ededed;
+}
+</style>
